2d026f7d8bcef216a2b3590c1e9e5176fde3c7e0
Chandrasekhar Kammara <Chandrasekhar.kammara@gmail.comgit config --global user.email Chandrasekhar.kammara@gmail.com>
2026-04-23 21:18:39 +0530

    ci

--- playwright.config.js
+++ playwright.config.js
@@ -2,11 +2,20 @@ const { defineConfig } = require('@playwright/test');
 const { ENV } = require('./config/env');
 
 module.exports = defineConfig({
+
   testDir: './tests',
+
+  // Reporter setup (HTML report enabled)
+  reporter: [
+    ['html', { open: 'never' }]
+  ],
 
   use: {
     baseURL: ENV.baseURL,
-    headless: false, // keep false for learning
+
+    // IMPORTANT: CI-safe mode
+    headless: true,
+
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
